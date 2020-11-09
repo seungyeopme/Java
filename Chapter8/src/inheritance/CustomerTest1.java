f530e82e0e861741f6bca1f60b759f5bda5b14c1
seungyeopme <seungyeop.me.dev@gmail.com>
2020-11-09 17:29:36 +0900

    Java

---
+++ Chapter8/src/inheritance/CustomerTest1.java
@@ -0,0 +1,17 @@
+package inheritance;
+
+public class CustomerTest1 {
+	public static void main(String[] args) {
+		
+//		Customer customerLee = new Customer();
+//		customerLee.setCustomerID(10100);
+//		customerLee.setCustomerName("Lee");
+		
+		Customer customerKim = new VIPCustomer(10101, "Kim", 100);
+		
+		customerKim.bonusPoint = 1000;
+		
+//		System.out.println(customerLee.showCustomerInfo());
+		System.out.println(customerKim.showCustomerInfo());
+	}
+}
