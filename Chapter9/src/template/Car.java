ef679fee626fa56dd0991e5033c88a27f6b253dc
seungyeopme <seungyeop.me.dev@gmail.com>
2020-11-10 15:59:56 +0900

    Java

---
+++ Chapter9/src/template/Car.java
@@ -0,0 +1,28 @@
+package template;
+
+public abstract class Car {
+
+	public abstract void drive();
+	public abstract void stop();
+	
+	public abstract void wiper();
+	
+	public void washCar() {}
+	
+	public void startCar() {
+		System.out.println("시동을 켭니다");
+	}
+	
+	public void turnOff() {
+		System.out.println("시동을 끕니다");
+	}
+	
+	final public void run() {
+		startCar();
+		drive();
+		wiper();
+		stop();
+		washCar();
+		turnOff();
+	}
+}
